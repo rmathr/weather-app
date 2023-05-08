83f643104b47220be7dd9c05615557575349ddc8
Matheus <85845749+rmathr@users.noreply.github.com>
2023-05-08 17:52:06 -0400

    Implement current weather&forecast and start styling

--- src/index.js
+++ src/index.js
@@ -1,26 +1,55 @@
 import "./style.css"
 import interactDOM from "./interactDOM"
+import { format } from 'date-fns'
 
 function handleSearch (){
     const cityInputForm = document.querySelector('form')
     const cityInput = document.getElementById('cityInput')
     let city
     cityInputForm.addEventListener('submit', async (e) =>{
-        e.preventDefault()
-        city = cityInput.value
-        const data = await searchWeather(city)
-        handleContentShow(data)
+        
+        if(!cityInput.validity.valid){
+            formValidation().showCityError()
+            e.preventDefault()
+        } else{
+            e.preventDefault()
+            city = cityInput.value
+
+            try{
+                const data = await searchWeather(city)
+                handleContentShow(data)
+           } catch(err){
+                const error = await err
+                formValidation().showCityError('err')
+                stardardLocation('toronto')
+           }finally{
+            interactDOM().formReset('searchCity')
+           }
+        }  
     })
 }
 
 handleSearch()
 
-function handleContentShow (obj){
-    // const currentWeather = interactDOM().createElementWithClassAndId('section', 'current-weather', 'currentWeather')
+function handleContentShow (obj, boolean){
     const currentWeather = interactDOM().hookDOMelement('currentWeather')
-    const mainContent = interactDOM().hookDOMelement('mainContent')
-    mainContent.appendChild(currentWeather)
-    interactDOM().appendElementAndDefineContent(currentWeather, obj)
+    const forecastWeather = interactDOM().hookDOMelement('forecastWeather')
+    const forecastOne = interactDOM().hookDOMelement('forecastOne')
+    const forecastTwo = interactDOM().hookDOMelement('forecastTwo')
+    const forecastThree = interactDOM().hookDOMelement('forecastThree')
+    const forecastFour = interactDOM().hookDOMelement('forecastFour')
+    const forecastFive = interactDOM().hookDOMelement('forecastFive')
+    forecastWeather.appendChild(forecastOne)
+    forecastWeather.appendChild(forecastTwo)
+    forecastWeather.appendChild(forecastThree)
+    forecastWeather.appendChild(forecastFour)
+    forecastWeather.appendChild(forecastFive)
+    interactDOM().appendElementAndDefineContentCurrent(currentWeather, obj, boolean)
+    interactDOM().appendElementAndDefineContentForecast(forecastOne, obj, 1, boolean)
+    interactDOM().appendElementAndDefineContentForecast(forecastTwo, obj, 2, boolean)
+    interactDOM().appendElementAndDefineContentForecast(forecastThree, obj, 3, boolean)
+    interactDOM().appendElementAndDefineContentForecast(forecastFour, obj, 4, boolean)
+    interactDOM().appendElementAndDefineContentForecast(forecastFive, obj, 5, boolean)
 }
 
 
@@ -32,16 +61,74 @@ async function searchWeather(searchCity){
     // const currentData = await currentWeather.json()
     // console.log(currentData)
     
-    const forecastWeather = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=005512f4aa2f45aab64154049230405&days=3&q=${searchCity}`)
-    const forecastData = await forecastWeather.json()
-    console.log(forecastData)
-    console.log(forecastData.location.name)
-    return forecastData
+        try{
+            const forecastWeather = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=005512f4aa2f45aab64154049230405&days=6&q=${searchCity}`)
+            const forecastData = await forecastWeather.json()
+            console.log(forecastData)
+            console.log(forecastData.current.condition.icon.replaceAll('/', ''))
+            console.log(format(new Date(forecastData.forecast.forecastday[1].date.replaceAll('-', '/')), "EEEE',' dd MMM"))
+            return forecastData
+        } catch(err){
+            return err
+        }
+
+
+        
     }
 
 async function stardardLocation(city) {
     const data = await searchWeather(city)
-    handleContentShow(data)
+    changeCelsiusFahrenheit(data)
+    // handleContentShow(data)
 }
 
 stardardLocation('toronto')
+
+
+function changeCelsiusFahrenheit(obj){
+    const toggleCelsiusFahrenheit = interactDOM().hookDOMelement('toggleCelsiusFahrenheit')
+    let unitStatus = true
+    handleContentShow(obj, unitStatus)
+    toggleCelsiusFahrenheit.addEventListener('mousedown', e => {
+        unitStatus ? false : true
+        handleContentShow(obj, unitStatus)
+    })
+}
+
+
+
+
+
+
+
+
+
+function formValidation(){
+    
+    const cityForm = document.getElementById('searchCity')
+    const cityInput = document.getElementById('cityInput')
+    const cityError = document.querySelector("#cityInput + span.city-error")
+
+    cityInput.addEventListener('input', e =>{
+        if(cityInput.validity.valid){
+            cityError.textContent = ""
+            cityError.className = "city-error"
+        } else {
+            showCityError()
+        }
+    })
+
+    function showCityError(err){
+        if(arguments.length > 0){
+            cityError.textContent = "No matching location found."
+        } else {
+            if(cityInput.validity.valueMissing){
+                cityError.textContent = "You need to enter a location."
+            } 
+        }
+        cityError.className = "city-error active"
+    }
+
+    return { showCityError }
+}
+formValidation()
--- src/interactDOM.js
+++ src/interactDOM.js
@@ -1,3 +1,5 @@
+import { format } from 'date-fns'
+
 const interactDOM = function(){
 
     const createElementWithClassAndId = function(type, className, idName){
@@ -34,27 +36,56 @@ const interactDOM = function(){
         element.style.display = 'flex'
     }
 
-    const appendElementAndDefineContent = function (container, obj) {
+    const appendElementAndDefineContentCurrent = function (container, obj) {
         while (container.hasChildNodes()){
         container.removeChild(container.firstChild)
         }
+        let currentCondition = interactDOM().createElementWithClassAndId('p', 'current-condition', 'currentCondition')
+        let currentImage = interactDOM().createElementWithClassAndId('img', 'current-image', 'currentImage')
         let locationElem = interactDOM().createElementWithClassAndId('p', 'location-name', 'locationName')
         let currentTemp = interactDOM().createElementWithClassAndId('p', 'current-temperature', 'currentTemperature')
         let currentFeelsLike = interactDOM().createElementWithClassAndId('p', 'current-feels-like', 'currentFellsLike')
         let currentWind = interactDOM().createElementWithClassAndId('p', 'current-wind', 'currentWind')
         let currentHumidity = interactDOM().createElementWithClassAndId('p', 'current-humidity', 'currentHumidity')
+        container.appendChild(currentCondition)
+        container.appendChild(currentImage)
         container.appendChild(locationElem)
         container.appendChild(currentTemp)
         container.appendChild(currentFeelsLike)
         container.appendChild(currentWind)
         container.appendChild(currentHumidity)
+        currentCondition.textContent = `${obj.current.condition.text}`
+        currentImage.src = `${obj.current.condition.icon.replaceAll('//', 'https://')}`
         locationElem.textContent = `${obj.location.name}, ${obj.location.country}`
-        currentTemp.textContent = `${obj.current.temp_c}`
-        currentFeelsLike.textContent = `${obj.current.feelslike_c}`
-        currentWind.textContent = `${obj.current.wind_kph}`
-        currentHumidity.textContent = `${obj.current.humidity}`
+        currentTemp.textContent = `${obj.current.temp_c}°C`
+        currentFeelsLike.textContent = `feels like: ${obj.current.feelslike_c}°C`
+        currentWind.textContent = `wind: ${obj.current.wind_kph} kph`
+        currentHumidity.textContent = `humidity: ${obj.current.humidity}%`
     }
 
+    const appendElementAndDefineContentForecast = function (container, obj, day) {
+        while (container.hasChildNodes()){
+        container.removeChild(container.firstChild)
+        }
+        let dayDate = interactDOM().createElementWithClassAndId('p', 'day-date', `dayDate#${obj.forecast.forecastday[day].date}`)
+        let maxTemp = interactDOM().createElementWithClassAndId('p', 'max-temperature', `maxTemperature#${obj.forecast.forecastday[day].date}`)
+        let minTemp = interactDOM().createElementWithClassAndId('p', 'min-temperature', `minTemperature#${obj.forecast.forecastday[day].date}`)
+        let conditionText = interactDOM().createElementWithClassAndId('p', 'forecast-condition', `forecastCondition#${obj.forecast.forecastday[day].date}`)
+        let chanceOfRain = interactDOM().createElementWithClassAndId('p', 'chance-rain', `chanceRain#${obj.forecast.forecastday[day].date}`)
+        let forecastImage = interactDOM().createElementWithClassAndId('img', 'forecast-image', `forecastImage#${obj.forecast.forecastday[day].date}`)
+        container.appendChild(dayDate)
+        container.appendChild(maxTemp)
+        container.appendChild(minTemp)
+        container.appendChild(conditionText)
+        container.appendChild(chanceOfRain)
+        container.appendChild(forecastImage)
+        dayDate.textContent = `${format(new Date(obj.forecast.forecastday[day].date.replaceAll('-', '/')), "EEEE',' dd MMM")}`
+        conditionText.textContent = `${obj.forecast.forecastday[day].day.condition.text}`
+        maxTemp.textContent = `${obj.forecast.forecastday[day].day.maxtemp_c}°C`
+        minTemp.textContent = `${obj.forecast.forecastday[day].day.mintemp_c}°C`
+        chanceOfRain.textContent = `chance of rain: ${obj.forecast.forecastday[day].day.daily_chance_of_rain}%`
+        forecastImage.src = `${obj.forecast.forecastday[day].day.condition.icon.replaceAll('//', 'https://')}`
+    }
 
     const formReset = function(formId){
     const form = interactDOM().hookDOMelement(`${formId}`)
@@ -65,7 +96,8 @@ const interactDOM = function(){
         createElementWithClassAndId, 
         hookDOMelement, 
         getInputValue, 
-        appendElementAndDefineContent,
+        appendElementAndDefineContentCurrent,
+        appendElementAndDefineContentForecast,
         toggleElementDisplay,
         hide,
         show,
